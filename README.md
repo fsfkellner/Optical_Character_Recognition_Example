34bda58b060e96e6694c78b89e34871b7567090c
Kellner <12001001994009@FEDIDCARD.GOV>
2020-10-08 13:02:53 -0600

    cleaned up notebooks improved helper functions and fixed typos in readme

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ## Using Optical Character Recognition to read text on aerial imagery
 This repository showcases my attempt to use optical character recognition to read text on an image and use this text to rename the file.  
-This repositiry can also be used as a tutorial for basic use of optical character recognition.
+This repository can also be used as a tutorial for basic use of optical character recognition.
 
 ## Getting Started
-Before you can use this repository locally you must first install tesseract  
-This is a stand alone application. For [Windows](https://github.com/UB-Mannheim/tesseract/wiki) installation of tesseract for [Linx and Mac](https://github.com/tesseract-ocr/tesseract/wiki#windows)
+Before you can use this repository locally you must first install tesseract a stand alone application.  
+For [Windows](https://github.com/UB-Mannheim/tesseract/wiki) installation of tesseract for [Linx and Mac](https://github.com/tesseract-ocr/tesseract/wiki#windows)
  
 Once tesseract is installled if you have Anaconda installation you can use the Anconda Prompt to install all of the python libraries  
 and their dependencies. In the Anaconda Prompt change directories so that you are in the local copy of this repository. In the prompt then enter: 
@@ -18,7 +18,7 @@ then enter:
 From here open the notebook entitled *ImageOCR.ipynb*
 
 ## Example:
-This image after it was scanned was given the filename *0F6185_0001_2*, my employer wanted to rename the file so that used the information in the top right of this image.  
+This image after it was scanned was given the filename *0F6185_0001_2*, my employer wanted to rename the file so that it used the information in the top right of this image.  
 Renaming the files in the manner would ultimately make them more searchable and useable for future use.  
 So rather than manually open each image and to see view this text I proposed using optical character recognition  
 to change the file name from *0F6185_0001_2.tif* to *DWW-A-5-1.tif*
--- helperFunctions.py
+++ helperFunctions.py
@@ -1,4 +1,4 @@
-from PIL import(
+from PIL import (
     Image,
     ImageFilter,
     ImageOps
@@ -7,13 +7,16 @@ import pytesseract
 from matplotlib import pyplot as plt
 import os
 import sys
-import zipfile
+from shutil import which
 
 
-def displayImage(image, figureTitle): 
-    plt.imshow(image, cmap='gray') # we set the keyword argument "cmap", which is the colormapping of our image to grayscale
-    plt.axis('off') # axes are not helpful in this instance
-    plt.title(figureTitle) # we'll use the title to remind us of any processing we did to our image 
+def displayImage(image, figureTitle):
+    # we set the keyword argument "cmap",
+    # which is the colormapping of our image to grayscale
+    plt.imshow(image, cmap='gray')
+    plt.axis('off')  # axes are not helpful in this instance
+    # we'll use the title to remind us of any processing we did to our image
+    plt.title(figureTitle) 
 
 
 def readTextAndPrint(image):
@@ -21,54 +24,56 @@ def readTextAndPrint(image):
         image,
         lang='eng',
         config='--psm 7 -c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-')
-    
+
     text = text.strip('\n')
     text = text.strip()
     print('The text on the image is being read as:', text)
 
-    
+
 def imageConvolution(file):
     img = Image.open(file)
     img = ImageOps.invert(img)
     imageWidth, imageHeight = img.size
     imagePixels = img.load()
-    
+
     for y in range(0, imageHeight):
         for x in range(0, imageWidth):
             if imagePixels[x, y] > 40:
                 imagePixels[x, y] = 255
             else:
                 pass
-    
+
     for y in range(0, imageHeight):
         for x in range(0, imageWidth):
             if imagePixels[x, y] <= 11:
                 imagePixels[x, y] = 0
             else:
                 pass
-            
+
     img = img.filter(ImageFilter.MinFilter(size=3))
     img = img.filter(ImageFilter.MinFilter(size=5))
-    
+
     return img
+
 
 def checkTesseractPath():
     if pytesseract.pytesseract.tesseract_cmd == 'tesseract':
-        from shutil import which
         if which('tesseract') is None and 'win' in sys.platform:
             try:
                 user = os.getlogin()
-                pytesseract.pytesseract.tesseract_cmd = r'C:\Users\{}\AppData\Local\Tesseract-OCR\tesseract.exe'.format(user) # this tesseract path on my Windows machine
+                # this tesseract path on my Windows machine
+                tesseractPath = r'C:\Users\{}\AppData\Local\Tesseract-OCR\tesseract.exe'.format(user)
+                pytesseract.pytesseract.tesseract_cmd = tesseractPath
+
             except Exception:
-                print('You need to find the location of the tesseract executable and set "pytesseract.pytesseract.tesseract_cmd" to this location, before you can proceed')
+                print('''You need to find the location of the tesseract executable 
+                and set "pytesseract.pytesseract.tesseract_cmd"
+                to this location, before you can proceed''')
+
         elif which('tesseract') is not None and 'win' not in sys.platform:
             pytesseract.pytesseract.tesseract_cmd = which('tesseract')
-        else:
-            print('You need to find the location of the tesseract executable and set "pytesseract.pytesseract.tesseract_cmd" to this location, before you can proceed')
 
-def unzipImages():
-    if os.path.exists(r'\images'):
-        pass
-    else:
-        with zipfile.ZipFile('images.zip', "r") as zipGDB:
-            zipGDB.extractall('images')
+        else:
+            print('''You need to find the location of the tesseract executable
+             and set "pytesseract.pytesseract.tesseract_cmd"
+             to this location, before you can proceed''')
